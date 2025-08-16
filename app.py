1821738dac893d996820d6779e580cc1fa881790
Marco Antonio Gelde <167691329+marcogelde@users.noreply.github.com>
2025-08-16 09:22:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 app = FastAPI()
+
 @app.get("/")
 def read_root():
- return {"message": "Olá turma, este é meu primeiro container com FastAPI!"}
+    return {"message": "Olá turma, este é meu primeiro container com FastAPI!"}
 
 @app.get("/marco")
-def read_root():
- return {"message": "Olá turma, este é o do Marco"}
+def read_marco():
+    return {"message": "Olá turma, este é o do Marco"}
